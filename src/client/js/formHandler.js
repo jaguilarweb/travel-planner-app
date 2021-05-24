633aeafa8b45ebb9444f06a4fc073503c2225df6
Jenny Aguilar <contact@jaguilarweb.com>
2021-05-23 22:09:38 -0400

    refactor: Change request geonames api to render specific data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,6 +3,7 @@ function handleSubmit(event){
 
   // check what text was put into the form field
   let formText = document.getElementById('location').value
+  
   const resultContainer = document.getElementById('results');
       fetch('http://localhost:8081/dataAnalyze',
       {
@@ -15,11 +16,18 @@ function handleSubmit(event){
       })
       .then(response => response.json())
       .then(function(response) {
-
+          console.log(response)
+        const latitude = response.geonames[0].lat;
+        const longitude = response.geonames[0].lng;
+        const country = response.geonames[0].countryName;
           resultContainer.className = "result-api";
           const paragraph = document.createElement('pre');
           resultContainer.appendChild(paragraph);
-          paragraph.innerHTML = JSON.stringify(response, null, 4);
+          //paragraph.innerHTML = JSON.stringify(response, null, 4);
+          paragraph.innerHTML = `<p> Lat: ${latitude} </p>
+                                <p> Lon: ${longitude} </p>
+                                <p>Country: ${country} </p>
+          `;
       })
       .catch(error => console.log(`Error: ${error}`));
 
--- src/server/server.js
+++ src/server/server.js
@@ -8,9 +8,7 @@ const axios = require('axios');
 dotenv.config();
 
 // Apis keys
-
-//const GEON_USER = process.env.GEON_USER;
-const GEON_USER = 'jaguilarweb';
+const GEON_USER = process.env.GEON_USER;
 
 //Config server express
 const app = express();
@@ -25,7 +23,8 @@ app.use(express.static('dist'));
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log("server running...");
-  console.log(`running on localhost:${port}`)
+  console.log(`running on localhost:${port}`);
+  console.log(GEON_USER)
 });
 
 
@@ -47,22 +46,11 @@ app.get('/', (req, res) => {
 /*---------------------------------
   GET Route
 ----------------------------------*/
-/* 
-app.post('/dataRetrieve', (req, res) => {
-//  const urltxt = req.body["formText"];
-  const location = 'london';
-
-  const response = fetch(`http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${GEON_USER}`)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(error => console.log('Error: ', error));
-}) */
 
 app.post('/dataAnalyze', async (req, res) => {
-//  const urltxt = req.body["formText"];
-//  const lang = "en";
+  const location = req.body["formText"];
   try {
-    const response = await axios.get(`http://api.geonames.org/searchJSON?q=london&maxRows=1&username=jaguilarweb`)
+    const response = await axios.get(`http://api.geonames.org/searchJSON?q=${location}&maxRows=1&username=${GEON_USER}`)
     res.send(response.data);
   } catch (error) {
     console.log('Error: ', error);
